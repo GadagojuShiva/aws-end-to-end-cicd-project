a9a4e7ef6212497e6bd46479c795655b07470cc7
Gadagoju Shiva <133100130+GadagojuShiva@users.noreply.github.com>
2024-02-07 16:12:11 +0530

    Update app.py

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return """
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f0f0f0;
+            text-align: center;
+            padding: 50px;
+        }
+
+        h1 {
+            color: #333;
+        }
+    </style>
+    <body>
+        <h1>Hello, world!</h1>
+        <p>Welcome to my beautiful Flask application!</p>
+        <pre>
+            .--.
+           |o_o |
+           |:_/ |
+          //   \ \
+         (|     | )
+        /'\_   _/`\
+        \___)=(___/
+
+        </pre>
+    </body>
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
